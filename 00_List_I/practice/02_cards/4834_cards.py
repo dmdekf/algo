dcdf2aea0918a9e2dab095ed22e196d94654550d
dmdekf <dmdekf@gmail.com>
2020-02-03 15:53:53 +0900

    Add 20200203_after

--- 00_List_I/practice/02_cards/4834_cards.py
+++ 00_List_I/practice/02_cards/4834_cards.py
@@ -7,7 +7,7 @@ T = int(input())
 for tc in range(1,T+1):
     n = int(input())
     d = list(input())
-    c = Counter(d).most_common()
+    c = Counter(d).most_common() # 빈도수, 가장 큰 값부터 정렬하여 list('value', count) return 
     max_num = c[0][1]
     max_cards = [x for x, y in c if y == max_num]
     print(f'#{tc} {max(max_cards)} {max_num}')
