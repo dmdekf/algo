a1e53ef2c0d75dd3eff92728ea856a3d192d644d
dmdekf <dmdekf@gmail.com>
2020-03-02 21:25:38 +0900

    Add 20200302_my_20_ver4

--- Algorithem_my/20/1861_ver4.py
+++ Algorithem_my/20/1861_ver4.py
@@ -7,16 +7,15 @@ def f(x, y, s, c):
     if v[d[x][y]]:
         return 1
     v[d[x][y]] = 1
-    temp[s] = c
+    v[s] = c
     for i in range(4):
         nx = x+dx[i]
         ny = y+dy[i]
         if 0 <= nx < N and 0 <= ny < N and d[nx][ny] == d[x][y] + 1:
             if f(nx, ny, s, c+1) == 1:
-                return c + temp[d[nx][ny]]
+                return c + v[d[nx][ny]]
             f(nx, ny, s, c+1)
-    return temp[s]
-    # return c
+    return v[s]
 
 
 T = int(input())
@@ -26,17 +25,16 @@ for tc in range(1, T+1):
     N = int(input())
     d = [list(map(int, input().split())) for _ in range(N)]
     v = [0]*(N**2+1)
-    temp = [0]*(N**2+1)
     cnt = float('-inf')
     for i in range(N):
         for j in range(N):
             if not v[d[i][j]] and cnt <= N**2 - d[i][j]:
-                temp[d[i][j]] = f(i, j, d[i][j], 1)
-                if cnt == temp[d[i][j]]:
+                v[d[i][j]] = f(i, j, d[i][j], 1)
+                if cnt == v[d[i][j]]:
                     if M > d[i][j]:
                         M = d[i][j]
-                elif cnt < temp[d[i][j]]:
-                    cnt = temp[d[i][j]]
+                elif cnt < v[d[i][j]]:
+                    cnt = v[d[i][j]]
                     M = d[i][j]
 
     print(f'#{tc} {M} {cnt}')
